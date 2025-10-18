380ea9bccc5f3822664131239d3c1a9f5e29b866
madalena6 <madalenapinadesousa@gmail.com>
2025-10-18 17:31:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function ligarVermelho() {
+  document.getElementById("semaforo").src = "Imagens/vermelho.png";
+}
+
+function ligarAmarelo() {
+  document.getElementById("semaforo").src = "Imagens/amarelo.png";
+}
+
+function ligarVerde() {
+  document.getElementById("semaforo").src = "Imagens/verde.png";
+}
+
+function desligar() {
+  document.getElementById("semaforo").src = "Imagens/desligado.png";
+}
